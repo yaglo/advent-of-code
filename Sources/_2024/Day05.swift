9ce79128bf62d4a96adbad24614b658094f890c4
Stanislav Yaglo <yaglo@me.com>
2024-12-05 17:07:52 +0000

    speed up isValid

--- Sources/_2024/Day05.swift
+++ Sources/_2024/Day05.swift
@@ -10,37 +10,37 @@ struct Day05: AdventDay {
   func part2() -> Int {
     updates
       .filter { !isValid($0) }
-      .map { $0.sorted(by: { pagePrecedence[$0][$1] }) }
+      .map { $0.sorted(by: { precedence[$0][$1]! }) }
       .sum { $0[$0.count / 2] }
   }
 
   func isValid(_ update: [Int]) -> Bool {
-    rules.allSatisfy { rule in
-      guard let index1 = update.firstIndex(of: rule.0), let index2 = update.firstIndex(of: rule.1)
-      else { return true }
-      return index1 < index2
+    update.indices.allSatisfy { i in
+      (i + 1..<update.count).allSatisfy { j in
+        precedence[update[i]][update[j]] ?? false
+      }
     }
   }
 
   // MARK: - Data
 
-  let rules: [(Int, Int)]
   let updates: [[Int]]
-  let pagePrecedence: [[Bool]]
+  let precedence: [[Bool?]]
 
   init(data: String) {
     let (r, u) = data.split(separator: "\n\n").splat()
-    rules = r.lines().map { $0.integers(separatedBy: "|").splat() }
+    let rules: [(Int, Int)] = r.lines().map { $0.integers(separatedBy: "|").splat() }
+
     updates = u.lines().map { $0.integers(separatedBy: ",") }
 
     let maxPageNumber =
       max(rules.flatMap { [$0.0, $0.1] }.max() ?? 0, updates.flatMap { $0 }.max() ?? 0) + 1
 
-    var pagePrecedence = Array(
-      repeating: Array(repeating: false, count: maxPageNumber), count: maxPageNumber)
+    var precedence = Array(
+      repeating: Array(repeating: Optional(false), count: maxPageNumber), count: maxPageNumber)
 
-    for rule in rules { pagePrecedence[rule.0][rule.1] = true }
+    for rule in rules { precedence[rule.0][rule.1] = true }
 
-    self.pagePrecedence = pagePrecedence
+    self.precedence = precedence
   }
 }
