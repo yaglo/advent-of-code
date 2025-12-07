32a62b914df53d90bd4db4191f1710071a7f3a17
Stanislav Yaglo <yaglo@me.com>
2025-12-07 13:25:11 +0000

    Day 7

--- Sources/_2025/Day07.swift
+++ Sources/_2025/Day07.swift
@@ -1,13 +1,40 @@
-// MARK: Day 7 -
+// MARK: Day 7: Laboratories -
 
 import AdventOfCode
 
 @Day struct Day07 {
+    let grid: [[Character]]
+
+    init(data: String) {
+        grid = data.mapLines { Array($0) }
+    }
+
+    func simulate() -> (splits: Int, paths: Int) {
+        var beamCounts = [grid[0].firstIndex(of: "S")!: 1]
+        var splits = 0
+
+        for row in 1..<grid.count {
+            var newCounts = [Int: Int]()
+            for (column, count) in beamCounts {
+                if grid[row][column] == "^" {
+                    splits += 1
+                    newCounts[column - 1, default: 0] += count
+                    newCounts[column + 1, default: 0] += count
+                } else {
+                    newCounts[column, default: 0] += count
+                }
+            }
+            beamCounts = newCounts
+        }
+
+        return (splits, beamCounts.sum(of: \.value))
+    }
+
     func part1() -> Int {
-        0
+        simulate().splits
     }
 
     func part2() -> Int {
-        0
+        simulate().paths
     }
 }
